51a233460859974a3e8ef4f3f8831766071382c3
irammlix <maferhallasi@gmail.com>
2026-06-14 14:49:46 -0500

    Modificacion en el Designer :v

--- CalculadoraBasica/FormCalculadora.Designer.cs
+++ CalculadoraBasica/FormCalculadora.Designer.cs
@@ -55,6 +55,7 @@
             // 
             this.tbCajaResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbCajaResultado.Location = new System.Drawing.Point(32, 72);
+            this.tbCajaResultado.MaximumSize = new System.Drawing.Size(0, 50);
             this.tbCajaResultado.Name = "tbCajaResultado";
             this.tbCajaResultado.ReadOnly = true;
             this.tbCajaResultado.Size = new System.Drawing.Size(410, 38);
@@ -67,9 +68,8 @@
             this.lblHistorial.AutoSize = true;
             this.lblHistorial.Location = new System.Drawing.Point(38, 36);
             this.lblHistorial.Name = "lblHistorial";
-            this.lblHistorial.Size = new System.Drawing.Size(11, 16);
+            this.lblHistorial.Size = new System.Drawing.Size(0, 16);
             this.lblHistorial.TabIndex = 1;
-            this.lblHistorial.Text = "-";
             // 
             // btnN7
             // 
@@ -285,6 +285,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
             this.ClientSize = new System.Drawing.Size(470, 592);
             this.Controls.Add(this.btnBorrar);
             this.Controls.Add(this.btnReset);
